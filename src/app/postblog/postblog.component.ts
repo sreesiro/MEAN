65e880214cfd4d6c0a3f4d04f7c5c35b0443bb45
Sreenath S <sreenathsirobhushanam@gmail.com>
2019-06-23 21:32:45 +0530

    Update of blog application

---
+++ src/app/postblog/postblog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postblog',
+  templateUrl: './postblog.component.html',
+  styleUrls: ['./postblog.component.css']
+})
+export class PostblogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
