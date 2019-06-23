4ea7135a5c9e4bea5109539386e1c5462ba88ccf
Sreenath S <sreenathsirobhushanam@gmail.com>
2019-06-23 22:14:01 +0530

    Update in Blog Application

--- Blog Appliation/src/app/app.component.spec.ts
+++ Blog Appliation/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Blog'`, () => {
+  it(`should have as title 'blog-FrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Blog');
+    expect(app.title).toEqual('blog-FrontEnd');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Blog!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to blog-FrontEnd!');
   });
 });
--- Blog Appliation/src/app/app.module.ts
+++ Blog Appliation/src/app/app.module.ts
@@ -1,19 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
+// Router Module, We have to inherit..
+import {RouterModule, Routes} from '@angular/router';
+
+// Import form module
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { AboutComponent } from './about/about.component';
+import { PostblogComponent } from './postblog/postblog.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+// import the blog service
+import { BlogService } from './blog.service';
+import { BlogDeleteComponent } from './blog-delete/blog-delete.component';
+import { BloghttpService } from './bloghttp.service';
 
+// The below line is called decorator..
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent,
+    AboutComponent,
+    PostblogComponent,
+    NotFoundComponent,
+    BlogDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'homeView/:blogId', component: HomeViewComponent},
+      {path: 'create', component: BlogCreateComponent},
+      {path: 'edit/:blogId', component: BlogEditComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'postblog', component: PostblogComponent},
+      {path: 'delete/:blogId', component: BlogDeleteComponent},
+      {path: '**', component: NotFoundComponent}
+    ])
   ],
-  providers: [],
+  providers: [BlogService, BloghttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
